98447001caf11f669c5ee15096d52d77eaf54cfe
Wioletta Kisielewicz <wioletta.kisielewicz@3shape.com>
2019-01-23 21:19:24 +0100

    First commit

--- SeleniumPageObjectPattern/Program.cs
+++ SeleniumPageObjectPattern/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumPageObjectPattern
 {
     class Program
     {
+
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
+            
+        }
+                
+        [SetUp]
+        public void Initialize()
+        {
+            
+            driver.Navigate().GoToUrl("https://www.ncbi.nlm.nih.gov/pubmed/");
+        }
+
+        [Test]
+        public void ExecuteTets()
+        {
+            SetMethods.SelectDropDown(driver, "database", "PMC", "Id");
+            SetMethods.EnterText(driver, "term", "genes", "Id");
+            SetMethods.Click(driver,"search", "Id");
+        }
+
+        [TearDown]
+        public void Clean()
+        {
+            driver.Close();
         }
     }
 }
---
+++ SeleniumPageObjectPattern/SetMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumPageObjectPattern
+{
+    class SetMethods
+    {
+        public static void EnterText(IWebDriver driver, string element, string value, string elementtype)
+        {
+            if (elementtype == "Id")
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).SendKeys(value);
+        }
+
+        public static void Click(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "Id")
+                driver.FindElement(By.Id(element)).Click();
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).Click();
+        }
+
+        public static void SelectDropDown(IWebDriver driver, string element, string value, string elementtype)
+        {
+            if (elementtype == "Id")
+               new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+            if (elementtype == "Name")
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+        }
+    }
+}
